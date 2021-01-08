9cf97c06d20d49c9c08f034d901fd398f41c7cbd
Alexander Kuznetsov <alex@beershire.ru>
2021-01-08 12:20:29 +0300

    Update and rename Pricer_beta_v0.2.py to Pricer_beta_v0.3.py

--- Pricing/Pricer_beta_v0.3.py
+++ Pricing/Pricer_beta_v0.3.py
@@ -19,7 +19,7 @@ def get_data(ticker, interval, start_date="2019-1-1", end_date='today'):
     # actual parcing
     data = yf.download(ticker, start_date, end_date, interval)['Adj Close']
     df_data = pd.DataFrame(data)
-    return df_data['Adj Close']   # data is a pd.Dataframe
+    return df_data['Adj Close']  # data is a pd.Dataframe
 
 
 def pricing(price_array, tau):
@@ -48,6 +48,23 @@ def pricing(price_array, tau):
 
     return fair_price
 
-print(get_data('AMZN', '60m'))
 
-print(pricing(get_data('AMZN', '60m'), 3))
+def runner(ticker, mult_long=0.5, mult_med=0.3, mult_short=0.2, start_date="2019-1-1", end_date='today'):
+    # Long_int
+    data_long = get_data(ticker, '1mo', start_date, end_date)
+    price_long = pricing(data_long, 3)
+    # Med_int
+    data_med = get_data(ticker, '1wk', start_date, end_date)
+    price_med = pricing(data_med, 5)
+    # Short_int
+    data_short = get_data(ticker, '1d', start_date, end_date)
+    price_short = (data_short, 7)
+    # Final weights
+    #end_price = mult_long * int(price_long) + mult_med * int(price_med) + mult_short * int(price_short)
+    # Test
+    #print(price_long)
+    #print(price_med)
+    #print(price_short)
+    #return end_price
+
+runner('AMZN')
