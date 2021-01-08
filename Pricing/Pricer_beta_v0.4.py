ed6050d48e1b73bb8aadca16e24fd7ae4a850ce0
Alexander Kuznetsov <alex@beershire.ru>
2021-01-08 12:30:34 +0300

    Update and rename Pricer_beta_v0.3.py to Pricer_beta_v0.4.py

--- Pricing/Pricer_beta_v0.4.py
+++ Pricing/Pricer_beta_v0.4.py
@@ -4,30 +4,26 @@ import pandas as pd
 import yfinance as yf
 from datetime import date
 
-"""# TEMP for testing from .csv
-data = pd.read_csv('Data.csv')
-price = data.Price"""
-
 
 def get_data(ticker, interval, start_date="2019-1-1", end_date='today'):
     # Today handling (obvious IMHO)
     if end_date == "today":
         today = date.today()
         end_date = str(today)
-    if OSError:
+    if OSError:   # FFS there is always an error
         print("Error: Dude something's wrong with your end_date")
     # actual parcing
     data = yf.download(ticker, start_date, end_date, interval)['Adj Close']
-    df_data = pd.DataFrame(data)
+    df_data = pd.DataFrame(data)   # this one is "just in case"
     return df_data['Adj Close']  # data is a pd.Dataframe
 
 
 def pricing(price_array, tau):
-    # bol stuff
     # Tell-a-trend
-    MA_price = price_array.rolling(tau).mean()
-    MA_price.fillna(price_array, inplace=True)
-
+    MA_price = price_array.rolling(tau).mean()   # Rolling mean of tau window
+    MA_price.fillna(price_array, inplace=True)   # Rolling mean(right) removes some data, so we replace it
+    
+    # algorithm for pricing (readme.md for more info)
     if (price_array[len(price_array) - 1]) - (MA_price[len(MA_price) - 1]) > np.std(price_array):
         Trend = True
         if (price_array[len(price_array) - 1]) - (MA_price[len(MA_price) - 1]) > 0:
@@ -49,22 +45,26 @@ def pricing(price_array, tau):
     return fair_price
 
 
-def runner(ticker, mult_long=0.5, mult_med=0.3, mult_short=0.2, start_date="2019-1-1", end_date='today'):
+def runner(ticker, mult_long=0.333, mult_med=0.333, mult_short=0.333, start_date="2019-1-1",
+           end_date='today'):  # the weights are taken as arguments
     # Long_int
     data_long = get_data(ticker, '1mo', start_date, end_date)
-    price_long = pricing(data_long, 3)
+    price_long = pricing(data_long, 3)   # manual tau
     # Med_int
     data_med = get_data(ticker, '1wk', start_date, end_date)
-    price_med = pricing(data_med, 5)
+    price_med = pricing(data_med, 5)   # manual tau
     # Short_int
     data_short = get_data(ticker, '1d', start_date, end_date)
-    price_short = (data_short, 7)
+    price_short = pricing(data_short, 7)   # manual tau
     # Final weights
-    #end_price = mult_long * int(price_long) + mult_med * int(price_med) + mult_short * int(price_short)
+    end_price = mult_long * int(price_long) + mult_med * int(price_med) + mult_short * int(price_short)
     # Test
-    #print(price_long)
-    #print(price_med)
-    #print(price_short)
-    #return end_price
 
-runner('AMZN')
+    return end_price
+
+
+####################################
+######### THE MAGIC BUTTON #########
+####################################
+
+print(runner('AMZN'))  # enter the ticker to launch the thing
